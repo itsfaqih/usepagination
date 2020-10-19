fda36e086ec1a85e2f0075b2fdae8346b4f1e2d5
Faqih Muntashir <itsfaqih@gmail.com>
2020-10-20 00:10:20 +0700

    Updates

---
+++ precache-manifest.7619514c8c4d98b807903db08497391c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e1d149219dc9c134e13af4c6e1458d",
+    "url": "/usepagination/index.html"
+  },
+  {
+    "revision": "c4fbbe31b40cf1c86f94",
+    "url": "/usepagination/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "32f9604bdaab5c2ea749",
+    "url": "/usepagination/static/js/2.c190a494.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/usepagination/static/js/2.c190a494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fbbe31b40cf1c86f94",
+    "url": "/usepagination/static/js/main.d5d607b5.chunk.js"
+  },
+  {
+    "revision": "4b5e89371cbf011f6f5c",
+    "url": "/usepagination/static/js/runtime-main.aebce0da.js"
+  }
+]);
